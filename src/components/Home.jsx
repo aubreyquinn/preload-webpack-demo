88fabcd506a7b772807310064493a8797026f85c
Aubrey Quinn <aubreyquinn@microsoft.com>
2022-10-21 15:30:38 +0100

    preload attempt

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Flowers from "../assets/flowers.jpg";
 
+let Lilies;
+
+import(/* webpackPrefetch: true */ "../assets/lilies.jpg").then((result) => { Lilies = result.default; console.log(result.default); } );
+
+// import Lilies from "../assets/lilies.jpg";
+
 // Home component
 const Home = () => {
   return <>
         <div>React Application</div>
-        <img src={Flowers} />
+        <img src={Flowers} height={200} />
+        <img src={Lilies} height={200} />
     </>;
 };
 
