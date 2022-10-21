b1b2d1c5074393e7762a5fab52c46467273d0a9e
Aubrey Quinn <aubreyquinn@microsoft.com>
2022-10-21 14:59:49 +0100

    updated assets

---
+++ src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Flowers from "../assets/flowers.jpg";
+
+// Home component
+const Home = () => {
+  return <>
+        <div>React Application</div>
+        <img src={Flowers} />
+    </>;
+};
+
+export default Home;
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,33 @@
-const path = require('path');
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: "none",
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
   },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
 };
